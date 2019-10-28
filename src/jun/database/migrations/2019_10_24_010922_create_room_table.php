68614e4f67adcd3d2a665efe5ffefe9d7861c418
宮田英信 <miyataeishin@garis-MacBook-Air.local>
2019-10-28 09:29:35 +0900

    laravel install.

---
+++ src/jun/database/migrations/2019_10_24_010922_create_room_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoomTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('room', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('country', 100);
+            $table->string('prefecture', 10);
+            $table->string('city', 10);
+            $table->string('facility', 255);
+            $table->string('facility', 255);
+            $table->double('lat', 8, 6);
+            $table->double('lng', 9, 6);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('room');
+    }
+}
